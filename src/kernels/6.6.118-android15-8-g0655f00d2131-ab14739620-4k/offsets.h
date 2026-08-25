9463ff098537f58d304d88c571f5d260d68b47e7
vn-ncvinh <vn.ncvinh@gmail.com>
2026-08-25 10:09:29 +0700

    add reno 16

---
+++ src/kernels/6.6.118-android15-8-g0655f00d2131-ab14739620-4k/offsets.h
@@ -0,0 +1,23 @@
+/* 6.6.118-android15-8-g0655f00d2131-ab14739620-4k */
+
+OFFSETS_ENTRY(
+    "6.6.118-android15-8-g0655f00d2131-ab14739620-4k",
+    STRUCT_OFFSETS_6_6,
+    .pselect_waiter_shift = -2,
+    .off_init_task = 0x0211e280,
+    .off_init_cred = 0x02130748,
+    .off_root_task_group = 0x02316580,
+    .off_selinux_enforcing = 0x02357ee0,
+    .off_selinux_blob_sizes = 0x016799f0,
+    .off_security_hook_heads = 0x016792b8,
+    .off_slide_nfulnl_logger = 0x02112260,
+    .off_slide_boot_id = 0x02378ed8,
+    .off_slide_loggers_0_1 = 0x021121b0,
+),
+
+/* BTF reference (runtime uses target.h defaults): */
+/* #define STRUCT_PAGE_SIZE 0x40 */
+/* #define STRUCT_PAGE_COMPOUND_HEAD 0x8 */
+/* #define STRUCT_PAGE_TYPE 0x30 */
+/* #define STRUCT_SLAB_CACHE 0x8 */
+/* #define STRUCT_MM_STRUCT 0x4C0 */
